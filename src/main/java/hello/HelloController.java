a9b5a312f3c524aed5f61394f058889483e02024
Alex Y <alex.yarochkin@gmail.com>
2015-12-14 12:50:45 -0800

    Added

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,14 +2,25 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.*;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        
+	return "Greetings from Spring Boot!";
+	
     }
+    @RequestMapping("/test")
+	public String test()
+	{
+		Date NewDate = new Date();	
+		
+		String resp = ("Key date:"+NewDate.toString());
+		return resp;
+	}
 
 }
 
