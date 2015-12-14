086d96e38539e6110c0a31f3a41047b8c8433ea9
root <root@pxe.amp.bigtdata.io>
2015-12-14 12:03:43 -0800

    Added InfoController

---
+++ src/main/java/hello/InfoController.groovy
@@ -0,0 +1,21 @@
+package hello
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class InfoController
+{
+  @Value('${spring.application.name}')
+  private String appName = "";
+  
+  @RequestMapping("/info")
+  public @ResponseBody Map<String, Object> info()
+  {
+    def resp = ["spring-application-name":appName];
+    
+    return resp;
+  }
+}
